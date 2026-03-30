5c6959a80d28b0c11b57b01ef5968f16c1645fec
Dencel K Babu <dencelbabu@gmail.com>
2026-03-30 17:30:47 +0530

    chore(ui): add dual-license headers to Dart source files
    - add copyright and SPDX license identifier to all Dart files.
    
    SPDX-License-Identifier: GPL-3.0-or-later OR LicenseRef-Commercial
    
    Signed-off-by: Dencel K Babu <dencelbabu@gmail.com>

--- app/lib/ui/diagnostics/diagnostics_logger.dart
+++ app/lib/ui/diagnostics/diagnostics_logger.dart
@@ -1,3 +1,5 @@
+// Copyright (c) 2026 Peters Digital
+// SPDX-License-Identifier: GPL-3.0-or-later OR LicenseRef-Commercial
 import 'dart:collection';
 
 import 'package:flutter/scheduler.dart';
--- app/lib/ui/midi_service.dart
+++ app/lib/ui/midi_service.dart
@@ -1,3 +1,5 @@
+// Copyright (c) 2026 Peters Digital
+// SPDX-License-Identifier: GPL-3.0-or-later OR LicenseRef-Commercial
 import 'dart:async';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
