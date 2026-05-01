252f1f94dd2e29cf09e25209b9a8665fb7ad3a4d
Dencel K Babu <dencelbabu@gmail.com>
2026-05-02 04:00:21 +0530

    refactor(core/models): cleanup PresetSnapshot and remove legacy configs
    - Remove DrumPadConfig and XYPadConfig from PresetSnapshot.
    - These are now integrated into the central LayoutState.
    - Note: Persistence of layout mappings per preset is pending
      further architectural refactor.

--- app/lib/core/models/preset_snapshot.dart
+++ app/lib/core/models/preset_snapshot.dart
@@ -1,25 +1,18 @@
 // Copyright (c) 2026 Peters Digital
 // SPDX-License-Identifier: GPL-3.0-or-later OR LicenseRef-Commercial
 
-import '../../ui/layout_state.dart' show DrumPadConfig, XYPadConfig;
 import 'control_state.dart';
 
 class PresetSnapshot {
   final ControlState controlState;
-  final Map<String, DrumPadConfig> drumPadConfigs;
-  final Map<String, XYPadConfig> xyPadConfigs;
 
   PresetSnapshot({
     required this.controlState,
-    required this.drumPadConfigs,
-    required this.xyPadConfigs,
   });
 
   Map<String, dynamic> toJson() {
     return {
       'controlState': controlState.toJson(),
-      'drumPadConfigs': drumPadConfigs.map((k, v) => MapEntry(k, v.toJson())),
-      'xyPadConfigs': xyPadConfigs.map((k, v) => MapEntry(k, v.toJson())),
     };
   }
 
@@ -28,18 +21,6 @@ class PresetSnapshot {
       controlState: ControlState.fromJson(
         json['controlState'] as Map<String, dynamic>,
       ),
-      drumPadConfigs:
-          (json['drumPadConfigs'] as Map<String, dynamic>?)?.map(
-            (k, v) =>
-                MapEntry(k, DrumPadConfig.fromJson(v as Map<String, dynamic>)),
-          ) ??
-          {},
-      xyPadConfigs:
-          (json['xyPadConfigs'] as Map<String, dynamic>?)?.map(
-            (k, v) =>
-                MapEntry(k, XYPadConfig.fromJson(v as Map<String, dynamic>)),
-          ) ??
-          {},
     );
   }
 }
